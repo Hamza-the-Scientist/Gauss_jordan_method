0b1f9f07cc3a80f31d15d6835b263f92e66c3201
Hamza <hrao01475@gmail.com>
2024-05-14 11:09:09 +0500

    Add Divide and Subtraction Function

--- Gauss_jordan_method.java
+++ Gauss_jordan_method.java
@@ -27,6 +27,44 @@ class Functions{
         }
         System.out.println();
         }
+
+        
+        // Divide the Row 1 first element ==> 1 
+        public static int[] Divide(int location, int[] Expression){
+            if(Expression[location] != 1){
+    
+                System.out.println("Row "+ (location+1) +" /" + Expression[location]);
+    
+                int New_Expression[] = new int[4];
+                for(int i = 0; i <= 3; i++){
+                New_Expression[i] = Expression[i] / Expression[location];
+                }
+                for(int i = 0; i <= 3; i++){
+                Expression[i] = New_Expression[i];
+                }
+            } 
+    
+            return Expression;
+        }
+    
+        //Subtract to get ==> 0
+        public static int[] Subtract(int location, int[] Expression,int[] Expression_1){
+            if(Expression[location] != 0 ){
+                System.out.println("Row 2 - ("+ Expression[location]+")Row 1");
+    
+                int[] New_Expression = new int[4];
+                for(int i = 0; i <= 3; i++){   
+                New_Expression[i] = (Expression[location]* Expression_1[i]);
+                }
+                // row2 = row2 - row1
+                for(int i = 0; i <= 3; i++){
+                Expression[i] = Expression[i] - New_Expression[i];
+                }
+            } 
+    
+    
+            return Expression;
+        }
     
 }
 
