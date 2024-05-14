380fcf38eabc91a28c86544e03712ecb6cad9226
Hamza <hrao01475@gmail.com>
2024-05-14 09:46:46 +0500

    Add initial files of Project

---
+++ Gauss_jordan_method.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Functions{
+    
+}
+
+// Main code-------------------------------------------------------------------------------
+public class Gauss_jordan_method{
+    public static void main(String args[]){
+        
+    }
+}
